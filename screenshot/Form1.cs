775b4b3d8af2caf1f114c59250c878c14ac1fb6e
omv0102 <omv98@outlook.com>
2020-03-25 22:34:40 +0700

    Добавьте файлы проекта.

---
+++ screenshot/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace screenshot
+{
+    public partial class Form1 : Form
+    {
+        int i = 0, imax = 10;
+        Bitmap bmp;
+        Graphics graph = null;
+        Random rnd = new Random();
+        Timer t = new Timer();
+
+        public Form1()
+        {
+            InitializeComponent();
+            bmp = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            // make unique folder to save screens
+            string dir;
+            do {
+                dir = rnd.Next(65536).ToString("X4");
+            } while(Directory.Exists(dir));
+            Directory.CreateDirectory(dir);
+            Directory.SetCurrentDirectory(dir);
+            // change max screens count if required
+            uint max;
+            if (Program.args.Length >= 1 && UInt32.TryParse(Program.args[0], out max))
+            {
+                imax = (int)max;
+            }
+            // start timer
+            t.Tick += new EventHandler(TickTimer);
+            t.Interval = 5000;
+            t.Start();
+        }
+
+        private void TickTimer(object sender, EventArgs ev)
+        {
+            try
+            {
+                graph = Graphics.FromImage(bmp);
+                graph.CopyFromScreen(0, 0, 0, 0, bmp.Size);
+                bmp.Save("screen" + (i++) + ".png", ImageFormat.Png);
+                if (i >= imax)
+                    Application.Exit();
+            }
+            catch (Exception e)
+            {
+                // ignore all exceptions
+                Console.WriteLine(e.Message);
+                Application.Exit();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Visible = false;
+            this.Enabled = false;
+        }
+    }
+}
---
+++ screenshot/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace screenshot
+{
+    static class Program
+    {
+        public static String[] args;
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main(String[] argv)
+        {
+            Program.args = argv;
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
